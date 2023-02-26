6e35beca21276d3dabce4640743e1ff31d6e187a
Sultan <seitmat.sultan@m4bank.com>
2023-02-26 16:57:51 +0600

    change to float

--- recipe/models.py
+++ recipe/models.py
@@ -77,7 +77,7 @@ class Recipe(models.Model):
 class Ingredient(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product",null=True)
     recipe = models.ForeignKey(Recipe, models.CASCADE, null=True)
-    count = models.IntegerField(default=0)
+    count = models.FloatField(default=0)
 
     def __str__(self):
         return self.product.name
